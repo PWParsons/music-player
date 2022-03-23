96f2ecc80ff68ae6d4427b152bf7d1a9e8751d0f
Peter <peterw.parsons@gmail.com>
2022-03-23 19:49:09 +0200

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+  import axios from 'axios'
+
+  const tuneIn = async () => {
+    return axios.get('https://coderadio-admin.freecodecamp.org/api/live/nowplaying/coderadio')
+      .then(response => response.data)
+  }
 </script>
 
 <main>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  {#await tuneIn()}
+    <p>Loading...</p>
+  {:then value}
+    <p>Welcome to {value.station.name}</p>
+  {:catch error}
+    <p>Something went wrong: {error}</p>
+  {/await}
 </main>
